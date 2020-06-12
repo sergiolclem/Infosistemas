de6b09f8f07e311b8a6da2d1dbc2b92a63e65993
Sérgio Luis Clem <sergiolclem@gmail.com>
2020-06-12 10:50:13 -0300

    install mocha

--- backend/src/app/controllers/cars.ctrl.js
+++ backend/src/app/controllers/cars.ctrl.js
@@ -1,59 +1,53 @@
 // Interactors
-const carCreate = require('../interectors/car-create.bs');
-const carList = require('../interectors/car-list.bs');
-const carDetail = require('../interectors/car-detail.bs');
-const carUpdate = require('../interectors/car-update.bs');
-const carDelete = require('../interectors/car-delete.bs');
-
-function createCar(req, res) {
-    try {
-        carCreate.execute(req, res);
-    } catch (error) {
-        console.log(error);
-        res.status(500);
-    }
-}
-
-function listCars(req, res) {
-    try {
-        carList.execute(req, res);
-    } catch (error) {
-        console.log(error);
-        res.status(500);
-    }
-}
-
-function getCar(req, res) {
-    try {
-        carDetail.execute(req, res);
-    } catch (error) {
-        console.log(error);
-        res.status(500);
-    }
-}
-
-function updateCar(req, res) {
-    try {
-        carUpdate.execute(req, res);
-    } catch (error) {
-        console.log(error);
-        res.status(500);
-    }
-}
-
-function deleteCar(req, res) {
-    try {
-        carDelete.execute(req, res);
-    } catch (error) {
-        console.log(error);
-        res.status(500);
-    }
-}
+const carCreate = require('../interactors/car-create.bs');
+const carList = require('../interactors/car-list.bs');
+const carDetail = require('../interactors/car-detail.bs');
+const carUpdate = require('../interactors/car-update.bs');
+const carDelete = require('../interactors/car-delete.bs');
 
 module.exports = {
-    createCar,
-    listCars,
-    getCar,
-    updateCar,
-    deleteCar
+    createCar: (req, res) => {
+        try {
+            carCreate.execute(req, res);
+        } catch (error) {
+            console.log(error);
+            res.status(500);
+        }
+    },
+
+    listCars: (req, res) => {
+        try {
+            carList.execute(req, res);
+        } catch (error) {
+            console.log(error);
+            res.status(500);
+        }
+    },
+
+    getCar: (req, res) => {
+        try {
+            carDetail.execute(req, res);
+        } catch (error) {
+            console.log(error);
+            res.status(500);
+        }
+    },
+
+    updateCar: (req, res) => {
+        try {
+            carUpdate.execute(req, res);
+        } catch (error) {
+            console.log(error);
+            res.status(500);
+        }
+    },
+
+    deleteCar: (req, res) => {
+        try {
+            carDelete.execute(req, res);
+        } catch (error) {
+            console.log(error);
+            res.status(500);
+        }
+    }
 }
